abee8bc256eb6074df33b8a7e6dd578bff4dc2a7
Angel Yair Monroy Silva <angelyairmonroy795@gmail.com>
2025-11-08 13:53:26 -0600

    Correcciones carpetas inecesaria

--- src/models/Producto.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const productoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  precio: { type: Number, required: true },
-  categoria: { type: String, default: "General" }
-}, { timestamps: true });
-
-export default mongoose.model("Producto", productoSchema);
