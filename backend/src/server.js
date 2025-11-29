7486b9d8fde9169acadd03aac73d43e63c3a8890
Angel Yair Monroy Silva <angelyairmonroy795@gmail.com>
2025-11-29 16:23:36 -0600

    correccion de fechaa

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-//  Permitir todos los orígenes temporalmente (debug)
 app.use(
   cors({
     origin: "*",
@@ -18,25 +17,20 @@ app.use(
   })
 );
 
-//  Middleware
 app.use(express.json());
 
-// Conexión a MongoDB
+// Conexión correcta
 mongoose
- mongoose.connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log(" Conectado a MongoDB Atlas"))
   .catch((err) => console.error(" Error al conectar con MongoDB:", err));
 
-
-//  Rutas
 app.use("/api/productos", productosRoutes);
 
-// Ruta raíz
 app.get("/", (req, res) => {
   res.send(" Servidor backend funcionando correctamente con CORS abierto");
 });
 
-//  Iniciar servidor
 app.listen(PORT, "0.0.0.0", () => {
   console.log(` Servidor corriendo en el puerto ${PORT}`);
 });
